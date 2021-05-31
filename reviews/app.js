141bbc593a00599ffb66a264718275d1425313b7
KobbyKnight <22242495+KobbyKnight@users.noreply.github.com>
2021-05-31 17:51:51 +0000

    reviews

--- reviews/app.js
+++ reviews/app.js
@@ -37,3 +37,66 @@ const reviews = [
       "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
   },
 ];
+
+const image = document.getElementById("person-img")
+const author = document.getElementById("author");
+const job = document.getElementById("job");
+const info = document.getElementById("info");
+
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+const randomBtn = document.querySelector(".random-btn");
+
+// set starting item
+let index = 0;
+
+//  default from array
+window.addEventListener("DOMContentLoaded", function () {
+  getReview(index);
+});
+
+// used to update view with reviews based on id passed as a parameter
+const getReview = function (i) {
+  let indexItem = reviews[i];
+  image.src = indexItem.img;
+  author.textContent = indexItem.name;
+  job.textContent = indexItem.job;
+  info.textContent = indexItem.text;
+}
+
+// get next review button
+nextBtn.addEventListener("click", () => {
+  let i = ++index;
+  if (i > reviews.length - 1){
+    index = 0;
+    i = index;
+  }
+  getReview(i);
+});
+
+// get prev review button
+prevBtn.addEventListener("click", () => {
+  let i = --index;
+  if (i < 0){
+    index = reviews.length - 1;
+    i = index;
+  }
+  getReview(i);
+});
+
+// get random review
+randomBtn.addEventListener("click", () => {
+  let randomIndex = Math.floor(Math.random() * reviews.length)
+  getReview(randomIndex);
+});
+
+// Button Press on keyboard
+const rightPress = new Event('click');
+const leftPress = new Event('click');
+window.addEventListener("keydown", (e) => {
+  if (e.key === "ArrowRight")
+    nextBtn.dispatchEvent(rightPress);
+  
+  if (e.key === "ArrowLeft")
+    prevBtn.dispatchEvent(leftPress);
+});
